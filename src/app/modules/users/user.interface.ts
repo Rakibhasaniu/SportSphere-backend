fae7a28362799518e88ab20b55aaceaeed9e1398
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-06-25 17:37:52 +0600

    user model created

---
+++ src/app/modules/users/user.interface.ts
@@ -0,0 +1,12 @@
+
+export type TUser = {
+    name:string;
+    username:string;
+    email:string;
+    password:string;
+}
+
+export type TLoginUser = {
+    username:string;
+    password:string;
+}
