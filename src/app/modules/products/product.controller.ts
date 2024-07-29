a64559374df00fb08a12bd15533c033198082f7b
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-07-29 16:10:16 +0600

    searching implemented

--- src/app/modules/products/product.controller.ts
+++ src/app/modules/products/product.controller.ts
@@ -18,7 +18,7 @@ const addProduct  = catchAsync(async(req:Request,res:Response)=> {
 const getAllProduct  = catchAsync(async(req:Request,res:Response)=> {
     // console.log(req.user)
     
-    const result = await ProductServices.getAllProductFromDB();
+    const result = await ProductServices.getAllProductFromDB(req);
 
     sendResponse(res,{
         success:true,
--- src/app/modules/products/product.service.ts
+++ src/app/modules/products/product.service.ts
@@ -1,6 +1,7 @@
-import { Types } from "mongoose";
+import { SortOrder, Types } from "mongoose";
 import { TProduct } from "./product.interface";
 import { Product } from "./product.model";
+import { Request } from "express";
 
 
 const addProductIntoDB = async(payload:TProduct) => {
@@ -9,11 +10,32 @@ const addProductIntoDB = async(payload:TProduct) => {
 
 
 }
-const getAllProductFromDB = async() => {
-    const result = await Product.find();
+const getAllProductFromDB = async(req:Request) => {
+  const pageM = Number(req.query.page) ? Number(req.query.page) - 1 : 0;
+  const limitM = Number(req.query.limit) ? Number(req.query.limit) : 5;
+  const search = req.query.search || "";
+  const sort = req.query.sort ? req.query.sort.toString() : "price";
+  // let genre = req.query.genre || "All";
+
+  const searchCriteria = {
+    $or: [
+      { name: { $regex: search, $options: "i" } },
+      { sportsType: { $regex: search, $options: "i" } },
+      { condition: { $regex: search, $options: "i" } },
+      { brand: { $regex: search, $options: "i" } },
+      
+    ]
+  };
+  // let sortBy: { [key: string]: SortOrder  } = {};
+  // const sortParts = sort.split(':');
+  // if (sortParts.length === 2) {
+  //   sortBy[sortParts[0]] = sortParts[1] as SortOrder;
+  // } else {
+  //   sortBy[sort] = "asc";
+  // }
+  // console.log(sortBy)
+    const result = await Product.find(searchCriteria).skip(page * limit);
     return result;
-
-
 }
 const getSingleProductFromDB = async(id:string) => {
     const result = await Product.findById(id);
